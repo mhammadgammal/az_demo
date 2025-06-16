bc647820114fb146ffc76b263a3a81a60e7ed8e0
mhammadgammal <mhammadgamal17@gmail.com>
2025-06-17 00:41:58 +0300

    login button done

--- lib/features/login/widgets/login_button.dart
+++ lib/features/login/widgets/login_button.dart
@@ -1,3 +1,4 @@
+import 'package:az_demo/core/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class LoginButton extends StatelessWidget {
@@ -16,8 +17,38 @@ class LoginButton extends StatelessWidget {
       width: double.infinity,
       child: ElevatedButton(
         onPressed: onPressed,
-        child: isLoading ? CircularProgressIndicator() : Text('Login'),
+        style: ElevatedButton.styleFrom(
+          elevation: isLoading ? 0.0 : null,
+          backgroundBuilder:
+              isLoading
+                  ? (context, set, child) =>
+                      onButtonLoading(context, set, child)
+                  : null,
+        ),
+        child: Text('Login'),
       ),
     );
   }
+
+  onButtonLoading(BuildContext context, Set<WidgetState> set, Widget? child) =>
+      Container(
+        width: double.infinity,
+        height: 30.0,
+        padding: EdgeInsets.symmetric(vertical: 5.0),
+        decoration: BoxDecoration(
+          color: AppColors.lightBackground,
+          borderRadius: BorderRadius.circular(20.0),
+          border: Border.all(
+            color:
+                ElevatedButton.styleFrom().backgroundColor?.resolve(set) ??
+                Colors.black,
+            width: 1.0,
+          ),
+        ),
+        child: SizedBox(
+          width: 20.0,
+          height: 20.0,
+          child: Center(child: CircularProgressIndicator()),
+        ),
+      );
 }
