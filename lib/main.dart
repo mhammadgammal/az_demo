fc551494e92bc3db30e2ee088e884facd1f4ac8a
mhammadgammal <mhammadgamal17@gmail.com>
2025-06-16 06:45:03 +0300

    add bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:az_demo/az_demo.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc_observer.dart';
 
 void main() {
+  Bloc.observer = AzObserver();
   runApp(const AzDemo());
 }
