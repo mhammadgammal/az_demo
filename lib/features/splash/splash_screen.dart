d3e721bc514e7841574b8fa48872c1e696025818
mhammadgammal <mhammadgamal17@gmail.com>
2025-06-17 21:37:06 +0300

    splash_screen done

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -1,15 +1,45 @@
+import 'package:az_demo/core/assets/app_images.dart';
+import 'package:az_demo/core/router/app_navigator.dart';
+import 'package:az_demo/core/router/route_keys.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  double _opacity = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _opacity = 1;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return AnimatedCrossFade(
-      firstChild: SizedBox(),
-      secondChild: Placeholder(),
-      crossFadeState: CrossFadeState.showFirst,
-      duration: Duration(milliseconds: 300),
+    return Scaffold(
+      body: Center(
+        child: AnimatedOpacity(
+          onEnd: () {
+            AppNavigator.replaceWith(context, RouteKeys.onBoarding);
+          },
+          opacity: _opacity,
+          duration: const Duration(seconds: 2),
+          child: SizedBox(
+            width: 200,
+            height: 200,
+            child: Center(child: Image.asset(AppImages.logo)),
+          ),
+        ),
+      ),
     );
   }
 }
